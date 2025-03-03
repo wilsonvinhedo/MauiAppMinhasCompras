2b8c5f9926630386e442d8b8d605ba9a8bad1091
wilso <wilso@LICO>
2025-03-02 22:15:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppMinhasCompras;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
